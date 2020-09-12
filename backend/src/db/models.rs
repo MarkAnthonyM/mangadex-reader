546c65d2b786dbbf5e0c8ed4e899b22901a907b4
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-12 11:17:20 -0500

    Add NewManga struct

--- backend/src/db/models.rs
+++ backend/src/db/models.rs
@@ -1,1 +1,12 @@
 use super::schema::manga;
+
+#[derive(Insertable)]
+#[table_name = "manga"]
+pub struct NewManga<'a> {
+    pub title: &'a str,
+    pub authors: Option<Vec<String>>,
+    pub artists: Option<Vec<String>>,
+    pub genre_ids: Option<Vec<u8>>,
+    pub genre_names: Option<Vec<String>>,
+    pub url_link: String,
+}
