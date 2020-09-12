2a9cd75feab1b48c6c6c83de86f4f5dfe69ea644
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-12 14:14:53 -0500

    Implement standard help function

---
+++ backend/src/bin/cli_tool.rs
@@ -0,0 +1,11 @@
+use std::env;
+use backend::db::{ create_manga, establish_connection };
+
+fn help() {
+    println!("subcommand:");
+    println!("    new<manga>: create new manga listing");
+}
+
+fn main() {
+
+}
