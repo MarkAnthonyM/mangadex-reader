b1bcd83f241272a569fa7b4b207b8057fafd69f8
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-12 20:17:45 -0500

    Implement cli new_manga function

--- backend/src/bin/cli_tool.rs
+++ backend/src/bin/cli_tool.rs
@@ -6,6 +6,28 @@ fn help() {
     println!("    new<manga>: create new manga listing");
 }
 
-fn main() {
+fn new_manga(args: &[String]) {
+    if args.len() < 1 {
+        println!("new: missing <manga>");
+        help();
+        return;
+    }
+    
+    let conn = establish_connection();
+    create_manga(&conn, &args[0]);
+}
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        help();
+        return;
+    }
+
+    let subcommand = &args[1];
+    match subcommand.as_ref() {
+        "new" => new_manga(&args[2..]),
+        _ => help(),
+    }
 }
