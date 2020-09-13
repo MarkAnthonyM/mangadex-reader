493e859725e51c451b6a5762b3f3ad1992ce78ba
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-13 15:54:12 -0500

    Implement Manga query struct

--- backend/src/db/models.rs
+++ backend/src/db/models.rs
@@ -23,3 +23,14 @@ impl<'a> NewManga<'a> {
         }
     }
 }
+
+#[derive(Queryable)]
+pub struct Manga {
+    pub id: i32,
+    pub title: String,
+    pub authors: Option<Vec<String>>,
+    pub artists: Option<Vec<String>>,
+    pub genre_ids: Option<Vec<i32>>,
+    pub genre_names: Option<Vec<String>>,
+    pub url_link: String,
+}
