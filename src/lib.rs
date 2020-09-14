82e33a121e0f9571c33462515fafdc4b89721c3c
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-14 14:26:49 -0500

    Implement Manga struct in root lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,13 @@
 #[macro_use]
 extern crate serde;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Manga {
+    pub id: i32,
+    pub title: String,
+    pub authors: Option<Vec<String>>,
+    pub artists: Option<Vec<String>>,
+    pub genre_ids: Option<Vec<i32>>,
+    pub genre_names: Option<Vec<String>>,
+    pub url_link: String,
+}
