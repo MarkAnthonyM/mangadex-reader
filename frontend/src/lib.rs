c42be9381e53f32b9e9233c606519229d4d7bcb1
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-14 21:35:13 -0500

    Implement Model struct

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -2,6 +2,10 @@ extern crate seed;
 use seed::{ prelude::*, * };
 use mangadex_reader::{ JsonApiResponse, MangaJsonWrapper };
 
+struct Model {
+    task: Vec<MangaJsonWrapper>,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
