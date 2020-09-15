784d64bea5e8e167973e34b495768e7eb68604ef
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-14 21:40:49 -0500

    Implement update function

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -9,3 +9,15 @@ struct Model {
 enum Msg {
     FetchedMangas(Result<JsonApiResponse, FetchError>),
 }
+
+fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::FetchedMangas(Ok(mut result)) => {
+            model.Mangas.clear();
+            model.Mangas.append(&mut result.data);
+        },
+        Msg::FetchedMangas(Err(reason)) => {
+            log!(format!("Error fetching: {:?}", reason));
+        },
+    }
+}
