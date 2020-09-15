7225cee84d80a4d174a5d8f264ae44fd074a4278
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-14 21:43:32 -0500

    Implement seed view function

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -3,7 +3,7 @@ use seed::{ prelude::*, * };
 use mangadex_reader::{ JsonApiResponse, MangaJsonWrapper };
 
 struct Model {
-    Mangas: Vec<MangaJsonWrapper>,
+    mangas: Vec<MangaJsonWrapper>,
 }
 
 enum Msg {
@@ -13,11 +13,23 @@ enum Msg {
 fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
     match msg {
         Msg::FetchedMangas(Ok(mut result)) => {
-            model.Mangas.clear();
-            model.Mangas.append(&mut result.data);
+            model.mangas.clear();
+            model.mangas.append(&mut result.data);
         },
         Msg::FetchedMangas(Err(reason)) => {
             log!(format!("Error fetching: {:?}", reason));
         },
     }
+}
+
+fn view(model: &Model) -> Node<Msg> {
+    h1![
+        "Mangas",
+        ul![
+            model
+                .mangas
+                .iter()
+                .map(|t| li![ t.attributes.title.clone() ])
+        ],
+    ]
 }
