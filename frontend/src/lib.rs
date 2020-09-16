5d168c8b9faa69f09e775f65bf1a4af7670fb561
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-15 23:45:22 -0500

    Implement daily manga view/style

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -26,9 +26,10 @@ fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
 //      View
 // ------ ------
 
-fn view(model: &Model) -> impl IntoNodes<Msg> {
+fn view(_model: &Model) -> impl IntoNodes<Msg> {
     nodes![
         view_nav(),
+        view_main(),
     ]
 }
 
@@ -48,6 +49,48 @@ fn view_nav() -> Node<Msg> {
                 ]
             ],
             h1![C!["navbar-title"], "Manga Test Site"],
+        ],
+    ]
+}
+
+// ------ Main ------
+fn view_main() -> Node<Msg> {
+    div![
+        C!["container", "card-row"],
+        div![
+            C!["card-main"],
+            h6![
+                C!["card-header"],
+                "Featured Daily Manga",
+            ],
+            div![
+                C!["card-body"],
+                div![
+                    C!["card-body-title"],
+                    a!["Mock Manga"],
+                ],
+                div![
+                    C!["card-body-img"],
+                    img![
+                        attrs! {
+                            At::Src => "https://via.placeholder.com/200";
+                        }
+                    ],
+                ],
+                div![
+                    C!["card-body-info"],
+                    div![
+                        a!["Chapter mock"],
+                    ],
+                    div![
+                        C!["group"],
+                        a!["Mock Scans"],
+                    ],
+                    div![
+                        span!["Mock mins ago"],
+                    ],
+                ],
+            ],
         ],
     ]
 }
