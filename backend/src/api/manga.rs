66805b72bef27fee8421c81041f2105c18f460a6
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-18 01:07:20 -0500

    Remove mock data function

--- backend/src/api/manga.rs
+++ backend/src/api/manga.rs
@@ -15,17 +15,6 @@ pub struct MangaData {
 }
 
 impl Manga {
-    // Create mock data for testing purposes.
-    pub fn create_mock() -> Self {
-        DexManga {
-            id: 0,
-            chapter: "fake chapter".to_string(),
-            cover_url: "fake cover".to_string(),
-            group: "fake group".to_string(),
-            timestamp: "fake timestamp".to_string(),
-        }
-    }
-
     // Make request for manga, fill struct with result. Currently sync, aim to make async.
     pub fn populate() -> Result<Manga, reqwest::Error> {
         let request = reqwest::blocking::get("https://www.mangadex.org/api/manga/42186")?
