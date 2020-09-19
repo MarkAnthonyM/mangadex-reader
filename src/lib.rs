7acb1a57896a759944b6bf305222bb487c888763
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-19 05:30:44 -0500

    Fix up root crate Manga struct fields

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,32 @@ extern crate serde;
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Manga {
-    pub id: i32,
-    pub title: String,
-    pub authors: Option<Vec<String>>,
+    pub alt_names: Vec<String>,
     pub artists: Option<Vec<String>>,
-    pub genre_ids: Option<Vec<i32>>,
-    pub genre_names: Option<Vec<String>>,
-    pub url_link: String,
+    pub authors: Option<Vec<String>>,
+    pub comments: i32,
+    pub cover_url: String,
+    pub covers: Vec<String>,
+    pub demographic: i32,
+    pub description: String,
+    pub follows: i32,
+    // Maybe use an enum here
+    pub genres: Vec<i32>,
+    pub hentai: i32,
+    pub id: i32,
+    pub lang_flag: String,
+    pub lang_name: String,
+    // Figure out proper field datatypes
+    // pub links: MangaLinks
+    // pub rating: MangaRating,
+    // pub related: SomeType
+    pub status: i32,
+    pub title: String,
+    pub views: i32,
+    // These fields may be unnecessary
+    // pub genre_ids: Option<Vec<i32>>,
+    // pub genre_names: Option<Vec<String>>,
+    // pub url_link: String,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
