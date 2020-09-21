1c0b4a3385f009b32a4b894271fc1e54db1079aa
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-20 19:16:16 -0500

    Change cors allowed_headers field to accept all

--- backend/src/bin/backend.rs
+++ backend/src/bin/backend.rs
@@ -118,7 +118,9 @@ fn main() -> Result<(), Error> {
 
     let cors = rocket_cors::CorsOptions {
         allowed_origins,
-        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        // figure out what headers need to go here
+        // allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allowed_headers: AllowedHeaders::all(),
         allow_credentials: true,
         ..Default::default()
     }
