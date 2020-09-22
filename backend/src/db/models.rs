bf03b474c8b40fca19f2168322ce1e9090b267a3
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-21 19:41:09 -0500

    Replace prototype NewManga struct with more complete version

--- backend/src/db/models.rs
+++ backend/src/db/models.rs
@@ -1,14 +1,28 @@
 use super::schema::manga;
 
+// Insertable struct representing manga item
 #[derive(Insertable)]
 #[table_name = "manga"]
-pub struct NewManga<'a> {
-    pub title: &'a str,
-    pub authors: Option<Vec<String>>,
+pub struct NewMan {
+    pub alt_names: Vec<String>,
     pub artists: Option<Vec<String>>,
-    pub genre_ids: Option<Vec<i32>>,
-    pub genre_names: Option<Vec<String>>,
-    pub url_link: String,
+    pub authors: Option<Vec<String>>,
+    // Find out what type this should be
+    // pub chapters: Vec<Chapters>,
+    pub comments: i32,
+    pub cover_url: String,
+    pub covers: Vec<String>,
+    pub demographic: i32,
+    pub description: String,
+    pub follows: i32,
+    pub genres: Vec<i32>,
+    pub hentai: i32,
+    pub lang_flag: String,
+    pub lang_name: String,
+    pub manga_id: String,
+    pub status: i32,
+    pub title: String,
+    pub views: i32,
 }
 
 impl<'a> NewManga<'a> {
